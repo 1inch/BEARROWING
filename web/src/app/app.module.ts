569f7b26814053798af55565359392daa090ef58
deacix <info@deacix.de>
2019-08-24 03:51:55 +0200

    some fixes

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -6,7 +6,6 @@ import {ServiceWorkerModule} from '@angular/service-worker';
 import {environment} from '../environments/environment';
 import {HttpClientModule} from '@angular/common/http';
 import {DeviceDetectorModule} from 'ngx-device-detector';
-import {LoadingSpinnerComponent} from './loading-spinner/loading-spinner.component';
 import {BaseComponent} from './base/base.component';
 import {LoadingSpinnerModule} from './loading-spinner/loading-spinner.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
@@ -14,12 +13,13 @@ import {FormsModule} from '@angular/forms';
 import {ModalModule} from 'ngx-bootstrap';
 import {NgbToastModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {NoContentComponent} from './no-content/no-content.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        BaseComponent,
-        LoadingSpinnerComponent
+        NoContentComponent,
+        BaseComponent
     ],
     imports: [
         BrowserAnimationsModule,
