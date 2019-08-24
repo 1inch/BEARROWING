e1e08b0165ed23a40f8ad5398c47ad93760d6398
Anton Bukov <k06aaa@gmail.com>
2019-08-24 20:52:19 +0200

    Add a lot

--- contracts/ITokenizer.sol
+++ contracts/ITokenizer.sol
@@ -3,41 +3,40 @@ pragma solidity ^0.5.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./ILoanPool.sol";
-import "./ICERC20.sol";
-import "./LoanHolder.sol";
-import "./UniversalERC20.sol";
 
-contract ITokenization {
+interface ITokenizer {
+
+    function enterMarkets(
+        uint256 tokenId,
+        address controller,
+        address[] calldata cTokens
+    )
+        external
+        returns(bytes memory ret);
 
     function migrate(
         ILoanPool pool,
         IERC20 collateralToken,
         uint256 collateralAmount,
-        ICERC20 borrowedToken,
+        IERC20 borrowedToken,
         IERC20 borrowedUnderlyingToken,
         uint256 borrowedAmount,
         address msgSender
     )
         external;
 
-    function enterMarkets(
-        uint256 tokenId,
-        ICompoundController controller,
-        address[] calldata cTokens
-    )
-        external;
-
     function mint(
         uint256 tokenId,
-        ICERC20 cToken,
+        IERC20 cToken,
         IERC20 token,
         uint256 amount
     )
-        external;
+        external
+        payable;
 
     function redeem(
         uint256 tokenId,
-        ICERC20 cToken,
+        IERC20 cToken,
         IERC20 token,
         uint256 amount
     )
@@ -45,7 +44,7 @@ contract ITokenization {
 
     function borrow(
         uint256 tokenId,
-        ICERC20 cToken,
+        IERC20 cToken,
         IERC20 token,
         uint256 amount
     )
@@ -53,9 +52,10 @@ contract ITokenization {
 
     function repay(
         uint256 tokenId,
-        ICERC20 cToken,
+        IERC20 cToken,
         IERC20 token,
         uint256 amount
     )
-        external;
+        external
+        payable;
 }
